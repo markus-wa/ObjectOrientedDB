1b67f94c63d93137afa313f2d8765388a6ba409d
Markus <m.walther97@gmail.com>
2019-10-20 21:33:51 +0200

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 Version=$1
 Source=$2
 ApiKey=$3
@@ -7,4 +9,4 @@ ApiKey=$3
 curl -L -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 export NUGET='mono nuget.exe'
 $NUGET pack ./ObjectOrientedDB/ObjectOrientedDB.nuspec -Version $Version
-$NUGET push ObjectOrientedDB/bin/Release/ObjectOrientedDB.*.nupkg -k $ApiKey -s $Source
+$NUGET push ObjectOrientedDB/bin/Release/ObjectOrientedDB.*.nupkg -ApiKey $ApiKey -Source $Source
